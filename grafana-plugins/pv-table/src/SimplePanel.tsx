2b77d7df2360239b1611acb5dd8b257cc5774d1a
Xinyu Wu <xinyu.wu@csiro.au>
2021-02-09 12:37:18 +1100

    New plugin to display pv values

---
+++ grafana-plugins/pv-table/src/SimplePanel.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { PanelProps } from '@grafana/data';
+import { SimpleOptions } from 'types';
+import { css, cx } from 'emotion';
+import { stylesFactory } from '@grafana/ui';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+interface Props extends PanelProps<SimpleOptions> {}
+
+const rows = [
+  { name: 'Frozen yoghurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0 },
+  { name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3 },
+  { name: 'Eclair', calories: 262, fat: 16.0, carbs: 24, protein: 6.0 },
+  { name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3 },
+  { name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9 },
+];
+
+export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+  // const theme = useTheme();
+  const styles = getStyles();
+
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+  return (
+    <div
+      className={cx(
+        styles.wrapper,
+        css`
+          width: ${width}px;
+          height: ${height}px;
+        `
+      )}
+    >
+      <ThemeProvider theme={darkTheme}>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </ThemeProvider>
+    </div>
+  );
+};
+
+const getStyles = stylesFactory(() => {
+  return {
+    wrapper: css`
+      position: relative;
+    `,
+    svg: css`
+      position: absolute;
+      top: 0;
+      left: 0;
+    `,
+    textBox: css`
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      padding: 10px;
+    `,
+    table: css`
+      minwidth: 100%;
+    `,
+  };
+});
