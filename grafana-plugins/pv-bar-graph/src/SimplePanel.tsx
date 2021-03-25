ece271d553efdb95764836406ebb4ae9755c0b09
Xinyu Wu <xinyu.wu@csiro.au>
2021-03-26 09:35:59 +1100

    display multiple bars

--- grafana-plugins/pv-bar-graph/src/SimplePanel.tsx
+++ grafana-plugins/pv-bar-graph/src/SimplePanel.tsx
@@ -12,6 +12,7 @@ interface Props extends PanelProps<SimpleOptions> {}
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   // const theme = useTheme();
   const styles = getStyles();
+  const barMargin = 5;
 
   const barScale = d3
     .scaleLinear()
@@ -19,14 +20,19 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
     .nice()
     .range([0, height]);
 
-  let dataFrame = null;
-  let barHeight = 0;
+  let barWidth = width;
+  let dataFrame: any = null;
+  let barCount = 0;
 
   if (data.series.length > 0) {
     dataFrame = data.series[0];
-    let val = getValue(dataFrame, 'Value');
-    barHeight = barScale(parseFloat(val));
+    barCount = dataFrame.length;
+    if (barCount > 0) {
+      barWidth = (width - barMargin * barCount) / barCount;
+    }
   }
+
+  console.log('pv bar graph');
 
   return (
     <div
@@ -45,16 +51,20 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
       >
-        <g className="pv-bar">
-          <rect style={{ fill: `#BDBDBD` }} x={0} y={0} height={height} width={width} />
-          <rect
-            className={getValue(dataFrame, 'alarm_severity').toLowerCase()}
-            x={0}
-            y={height - barHeight}
-            height={barHeight}
-            width={width}
-          />
-        </g>
+        {Array(barCount)
+          .fill(0)
+          .map((_, index) => (
+            <g className="pv-bar">
+              <rect style={{ fill: `#BDBDBD` }} x={index * (barWidth + barMargin)} height={height} width={barWidth} />
+              <rect
+                className={getValue(dataFrame, 'alarm_severity', index).toLowerCase()}
+                x={index * (barWidth + barMargin)}
+                y={height - getScaledValue(getValue(dataFrame, 'Value', index), barScale)}
+                height={getScaledValue(getValue(dataFrame, 'Value', index), barScale)}
+                width={barWidth}
+              />
+            </g>
+          ))}
       </svg>
     </div>
   );
@@ -79,7 +89,16 @@ const getStyles = stylesFactory(() => {
   };
 });
 
-function getValue(dataFrame: DataFrame | null, fieldName: string): string {
+function getScaledValue(val: string, scale: any) {
+  let numVal = Number(val);
+  if (!isNaN(numVal)) {
+    return scale(numVal);
+  }
+
+  return 0;
+}
+
+function getValue(dataFrame: DataFrame | null, fieldName: string, index: number): string {
   if (dataFrame === null) {
     return '';
   }
@@ -88,7 +107,7 @@ function getValue(dataFrame: DataFrame | null, fieldName: string): string {
     if (field['name'] === fieldName) {
       const list = field.values;
       if (list && list.length > 0) {
-        let val = list.get(list.length - 1);
+        let val = list.get(index);
         return val!;
       }
     }
